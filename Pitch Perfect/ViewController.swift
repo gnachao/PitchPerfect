ab1ce227299cb0ea1ae8aa9d1cc165cdcfe1bcd3
MacBKPro <macbkpro@MacBKPros-MBP.hsd1.ca.comcast.net>
2015-07-09 17:47:01 -0700

    add stop button, hidden on willAppear, show when click recording button

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var RecordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        //Hide the stop button
+        stopButton.hidden = true
+    }
     @IBAction func recordAudio(sender: UIButton) {
-       
+       stopButton.hidden = false
         RecordingInProgress.hidden = false
         
         //TODO: Record the user's voice
