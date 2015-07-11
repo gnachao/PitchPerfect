5bcf78eccc3a612d29b8df6aa317f0f98a04e9d0
MacBKPro <macbkpro@MacBKPros-MBP.hsd1.ca.comcast.net>
2015-07-11 14:32:53 -0700

    add pause and resume features

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -17,34 +17,51 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     var audioRecorder: AVAudioRecorder!
     var recordedAudio: RecordedAudio!
-
+    
     override func viewWillAppear(animated: Bool) {
         // Not recording indicators
         stopButton.hidden = true
         recordButton.enabled = true
+        recordButton.setTitle("Recording", forState: UIControlState.Normal)
+        let image = UIImage(named: "Microphone.png") as UIImage!
+        recordButton.setImage(image, forState: .Normal)
     }
     
     @IBAction func recordAudio(sender: UIButton) {
         // Recording inicators
-        stopButton.hidden = false
-        RecordingInProgress.hidden = false
-        recordButton.enabled = false
-        
-        // Define recorded file path, recorded file name can be unique by using YYYYMMDDHHSS but not require for this project.
-        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
-        var pathArray = [dirPath, "recorded.wav"]
-        let filePath = NSURL.fileURLWithPathComponents(pathArray)
-        
-        // Setup audio session to Record Session only
-        var session = AVAudioSession.sharedInstance()
-        session.setCategory(AVAudioSessionCategoryRecord, error: nil)
-        
-        // Initialize and prepare the recorder
-        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
-        audioRecorder.delegate = self // to use audio functions in this class
-        audioRecorder.meteringEnabled = true;
-        audioRecorder.prepareToRecord()
-        audioRecorder.record()
+        switch recordButton.currentTitle! {
+            case "Recording":
+                setRecordingStage(recordButton.currentTitle!)
+                
+                // Define recorded file path, recorded file name can be unique by using YYYYMMDDHHSS but not require for this project.
+                let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+                var pathArray = [dirPath, "recorded.wav"]
+                let filePath = NSURL.fileURLWithPathComponents(pathArray)
+                
+                // Setup audio session to Record Session only
+                var session = AVAudioSession.sharedInstance()
+                session.setCategory(AVAudioSessionCategoryRecord, error: nil)
+                
+                // Initialize and prepare the recorder
+                audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+                audioRecorder.delegate = self // to use audio functions in this class
+                audioRecorder.meteringEnabled = true;
+                audioRecorder.prepareToRecord()
+                audioRecorder.record()
+
+                println("You pressed Recording")
+            case "Pausing":
+                setRecordingStage(recordButton.currentTitle!)
+                audioRecorder.pause()
+                println("You pressed Pausing")
+            case "Resuming":
+                setRecordingStage(recordButton.currentTitle!)
+                audioRecorder.record()
+                println("You pressed Resuming")
+        default:
+            println("The recording button title is not in the implementation!")
+            break
+        }
     }
 
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
@@ -69,6 +86,34 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
             let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
             let data = sender as! RecordedAudio
             playSoundsVC.receivedAudio = data
+        }
+    }
+    
+    // Set characteristics of each recording stage.
+    func setRecordingStage(stage: String){
+        switch stage {
+            case "Recording":
+                recordButton.setTitle("Pausing", forState: UIControlState.Normal)
+                stopButton.hidden = false
+                RecordingInProgress.hidden = false
+                let image = UIImage(named: "Pause.png") as UIImage!
+                recordButton.setImage(image, forState: .Normal)
+            case "Pausing":
+                recordButton.setTitle("Resuming", forState: UIControlState.Normal)
+                let image = UIImage(named: "Resume.png") as UIImage!
+                recordButton.setImage(image, forState: .Normal)
+                RecordingInProgress.hidden = true
+            case "Resuming":
+                recordButton.setTitle("Pausing", forState: UIControlState.Normal)
+                let image = UIImage(named: "Pause.png") as UIImage!
+                recordButton.setImage(image, forState: .Normal)
+                RecordingInProgress.hidden = false
+        default:
+            recordButton.setTitle("Recording", forState: UIControlState.Normal)
+            stopButton.hidden = true
+            RecordingInProgress.hidden = true
+            let image = UIImage(named: "Microphone.png") as UIImage!
+            recordButton.setImage(image, forState: .Normal)
         }
     }
     
